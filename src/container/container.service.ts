c537de90ad2aedb895f94301610a39a6ab0d9c2a
TarıkSogukpinar <tariksogukpinar@outlook.com>
2025-01-01 11:40:19 +0300

    feat(container) create & listed container init

--- src/container/container.service.ts
+++ src/container/container.service.ts
@@ -8,25 +8,53 @@ export class ContainerService {
 
     constructor() {
         // Local Docker daemon ile iletişim kuran Docker instance
-        this.dockerService = new Docker({ socketPath: '/var/run/docker.sock' });
+        this.dockerService = new Docker({
+            host: 'localhost', // Localhost üzerinden bağlanıyoruz
+            port: 2375,        // Docker Desktop'un açık TCP portu
+        });
     }
 
     async createAndStartContainer(options: ContainerCreateOptions): Promise<string> {
         try {
-          const container = await this.dockerService.createContainer(options);
-          await container.start();
-          return `Container ${container.id} created and started successfully.`;
+            await this.pullImage(options.Image);
+            const container = await this.dockerService.createContainer(options);
+            await container.start();
+            return `Container ${container.id} created and started successfully.`;
         } catch (error) {
-          throw new Error(`Failed to create and start container: ${error.message}`);
+            console.log(error);
+            throw new Error(`Failed to create and start container: ${error.message}`);
         }
-      }
-    
-      // 2. Tüm konteynerleri listeleme
-      async listContainers(): Promise<ContainerInfo[]> {
+    }
+
+    // 2. Tüm konteynerleri listeleme
+    async listContainers(): Promise<ContainerInfo[]> {
         try {
-          return await this.dockerService.listContainers();
+            return await this.dockerService.listContainers();
         } catch (error) {
-          throw new Error(`Failed to list containers: ${error.message}`);
+            console.log(error);
+            throw new Error(`Failed to list containers: ${error.message}`);
         }
-      }
+    }
+
+    private async pullImage(image: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.dockerService.pull(image, (err, stream) => {
+                if (err) {
+                    return reject(err);
+                }
+                this.dockerService.modem.followProgress(stream, onFinished, onProgress);
+
+                function onFinished(err) {
+                    if (err) {
+                        return reject(err);
+                    }
+                    resolve();
+                }
+
+                function onProgress(event) {
+                    console.log(event);
+                }
+            });
+        });
+    }
 }
