dd443bf8263a3b8cb9b7effddd9a7740ad2e643f
TarıkSogukpinar <tariksogukpinar@outlook.com>
2025-01-01 10:49:40 +0300

    chore(prisma) prisma connection init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: (() => {
+        const env = process.env.NODE_ENV;
+        const envFilePath =
+          env === 'production'
+            ? '.env.production'
+            : env === 'staging'
+              ? '.env.staging'
+              : '.env.development';
+        console.log(`Loading environment variables from ${envFilePath}`);
+        return envFilePath;
+      })(),
+    }),
+    PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
